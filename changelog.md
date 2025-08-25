22e60e250b77910120c22a8a6170fe9949b3dd28
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-08-25 16:15:32 -0400

    Wow.

--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
 # Closeable Alerts Changelog
-## v1.0.0
+## v1.0.1 (2025-08-25)
+- Fixed some potential issues with advanced popup closing behavior
+
+## v1.0.0 (2025-08-25)
 - Initial release
--- main.cpp
+++ main.cpp
@@ -3,20 +3,26 @@
 using namespace geode::prelude;
 
 class $modify(CAAlertLayer, FLAlertLayer) {
+    struct Fields {
+        bool m_overrideBack = false;
+    };
+
     static void onModify(ModifyBase<ModifyDerive<CAAlertLayer, FLAlertLayer>>& self) {
         (void)self.setHookPriority("FLAlertLayer::keyBackClicked", Priority::Replace);
     }
 
-    bool init(
-        FLAlertLayerProtocol* delegate, const char* title, gd::string desc, const char* btn1, const char* btn2,
-        float width, bool scroll, float unused, float textScale
-    ) {
-        if (!FLAlertLayer::init(delegate, title, desc, btn1, btn2, width, scroll, unused, textScale)) return false;
+    bool init(FLAlertLayerProtocol* p0, const char* p1, gd::string p2, const char* p3, const char* p4, float p5, bool p6, float p7, float p8) {
+        if (!FLAlertLayer::init(p0, p1, p2, p3, p4, p5, p6, p7, p8)) return false;
+
+        auto background = m_mainLayer->getChildByID("background");
+        if (!background) return true;
+
+        auto& contentSize = background->getContentSize();
+
+        m_fields->m_overrideBack = true;
 
         auto closeSprite = CCSprite::createWithSpriteFrameName("GJ_closeBtn_001.png");
         closeSprite->setScale(0.8f);
-
-        auto& contentSize = m_mainLayer->getChildByID("background")->getContentSize();
 
         auto closeButton = CCMenuItemSpriteExtra::create(closeSprite, this, menu_selector(CAAlertLayer::onCloseButton));
         closeButton->setPosition(m_buttonMenu->convertToNodeSpace(
@@ -35,21 +41,26 @@ class $modify(CAAlertLayer, FLAlertLayer) {
     }
 
     void keyBackClicked() {
-        onCloseButton(nullptr);
+        if (m_fields->m_overrideBack) onCloseButton(nullptr);
+        else FLAlertLayer::keyBackClicked();
     }
 };
 
 #ifdef GEODE_IS_MACOS
+uintptr_t keyBackClickedAddress = base::get() + GEODE_ARM_MAC(0x3f64cc) GEODE_INTEL_MAC(0x4890f0);
+
 void keyBackClickedHook(uintptr_t self) {
-    reinterpret_cast<CAAlertLayer*>(self - sizeof(CCNode) - sizeof(CCAccelerometerDelegate) - sizeof(CCTouchDelegate))->onCloseButton(nullptr);
+    auto layer = reinterpret_cast<CAAlertLayer*>(self - sizeof(CCNode) - sizeof(CCAccelerometerDelegate) - sizeof(CCTouchDelegate));
+    if (layer->m_fields->m_overrideBack) layer->onCloseButton(nullptr);
+    else reinterpret_cast<void(*)(uintptr_t)>(keyBackClickedAddress)(self);
 }
 
 $execute {
     auto hook = Hook::create(
-        reinterpret_cast<void*>(base::get() + GEODE_ARM_MAC(0x3f64cc) GEODE_INTEL_MAC(0x4890f0)),
+        reinterpret_cast<void*>(keyBackClickedAddress),
         &keyBackClickedHook,
         "FLAlertLayer::CCKeypadDelegate::keyBackClicked",
-        tulip::hook::TulipConvention::Thiscall
+        tulip::hook::TulipConvention::Default
     );
     hook->setPriority(Priority::Replace);
     Mod::get()->claimHook(hook).inspectErr([](const std::string& err) {
